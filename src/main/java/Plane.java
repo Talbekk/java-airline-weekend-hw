323e642032020b2bdc8c863a5bd3f0eb0f814b79
Talbekk <mt.campbell@outlook.com>
2020-01-10 14:31:06 +0000

    adds plane class, plane test file and basic tests and planetype enum file

---
+++ src/main/java/Plane.java
@@ -0,0 +1,12 @@
+public class Plane {
+
+    private PlaneType typeOfPlane;
+
+    public Plane(PlaneType typeOfPlane) {
+        this.typeOfPlane = typeOfPlane;
+    }
+
+    public PlaneType getTypeOfPlane() {
+        return this.typeOfPlane;
+    }
+}
---
+++ src/main/java/PlaneType.java
@@ -0,0 +1,8 @@
+public enum PlaneType {
+
+    BOEING747,
+    BOEING636,
+    BOEING858,
+    BOEING969
+
+}
